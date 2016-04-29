45c7f160b8ac6145790bc35fabcdea5a4ba5b9bf
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-04-29 22:50:09 +0200

    Tests écrits et validés

--- clotureFichesGSB/NUnit/GestionDatesTests.cs
+++ clotureFichesGSB/NUnit/GestionDatesTests.cs
@@ -21,14 +21,16 @@ namespace NUnit
         [Test]
         public void getMoisPrecedentTest2()
         {
-
+            DateTime mois = new DateTime(2015,10,02);
+            Assert.AreEqual("09", clotureFichesGSB.GestionDates.getMoisPrecedent(mois));
         }
 
         //Test mois précédent avec date particulière : janvier (donc mois = 01 moins un mois = 12)
         [Test]
         public void getMoisPrecedentTest3()
         {
-
+            DateTime mois = new DateTime(2015, 01, 02);
+            Assert.AreEqual("12", clotureFichesGSB.GestionDates.getMoisPrecedent(mois));
         }
 
         //Test mois suivant avec la date du jour
@@ -42,21 +44,23 @@ namespace NUnit
         [Test]
         public void getMoisSuivantTest2()
         {
-
+            DateTime mois = new DateTime(2015, 10, 02);
+            Assert.AreEqual("11", clotureFichesGSB.GestionDates.getMoisSuivant(mois));
         }
 
         //Test mois suivant avec date particulière : décembre (donc mois = 12 moins un mois = 01)
         [Test]
         public void getMoisSuivantTest3()
         {
-
+            DateTime mois = new DateTime(2015, 12, 02);
+            Assert.AreEqual("01", clotureFichesGSB.GestionDates.getMoisSuivant(mois));
         }
 
         //Test : le jour de la date du jour doit être compris entre les deux paramètres.
         [Test]
         public void entreTest1()
         {
-
+            Assert.IsTrue(clotureFichesGSB.GestionDates.entre(20,30)) ;
         }
 
 
@@ -64,21 +68,23 @@ namespace NUnit
         [Test]
         public void entreTest2()
         {
-
+            Assert.IsFalse(clotureFichesGSB.GestionDates.entre(7, 14)) ;
         }
 
         //Test : le jour de la date passée en paramètre doit être compris entre les deux paramètres.
         [Test]
         public void entreTest3()
         {
-
+            DateTime date = new DateTime(2015, 10, 15);
+            Assert.IsTrue(clotureFichesGSB.GestionDates.entre(date,10, 30));
         }
 
         //Test : le jour de la date passée en paramètre ne doit pas être compris entre les deux paramètres.
         [Test]
         public void entreTest4()
         {
-
+            DateTime date = new DateTime(2015, 10, 15);
+            Assert.IsFalse(clotureFichesGSB.GestionDates.entre(date,7, 14));
         }
     }
 }
