7e60ca407495de3b7077776046269e6222271929
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-04-29 22:41:18 +0200

    fin mise en place structure des tests de dates

--- clotureFichesGSB/NUnit/GestionDatesTests.cs
+++ clotureFichesGSB/NUnit/GestionDatesTests.cs
@@ -18,7 +18,65 @@ namespace NUnit
         }
 
         //Test mois précédent avec une date quelconque
+        [Test]
         public void getMoisPrecedentTest2()
+        {
+
+        }
+
+        //Test mois précédent avec date particulière : janvier (donc mois = 01 moins un mois = 12)
+        [Test]
+        public void getMoisPrecedentTest3()
+        {
+
+        }
+
+        //Test mois suivant avec la date du jour
+        [Test]
+        public void getMoisSuivantTest1()
+        {
+            Assert.AreEqual("05", clotureFichesGSB.GestionDates.getMoisSuivant());
+        }
+
+        //Test mois suivant avec une date quelconque
+        [Test]
+        public void getMoisSuivantTest2()
+        {
+
+        }
+
+        //Test mois suivant avec date particulière : décembre (donc mois = 12 moins un mois = 01)
+        [Test]
+        public void getMoisSuivantTest3()
+        {
+
+        }
+
+        //Test : le jour de la date du jour doit être compris entre les deux paramètres.
+        [Test]
+        public void entreTest1()
+        {
+
+        }
+
+
+        //Test : le jour de la date du jour ne doit pas être compris entre les deux paramètres.
+        [Test]
+        public void entreTest2()
+        {
+
+        }
+
+        //Test : le jour de la date passée en paramètre doit être compris entre les deux paramètres.
+        [Test]
+        public void entreTest3()
+        {
+
+        }
+
+        //Test : le jour de la date passée en paramètre ne doit pas être compris entre les deux paramètres.
+        [Test]
+        public void entreTest4()
         {
 
         }
